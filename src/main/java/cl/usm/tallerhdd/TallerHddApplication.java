27591d36d7b47bf0965a7c95a12df5ac9942f054
iari4as <iariass@usm.cl>
2026-06-23 21:44:06 -0400

    primer commit

---
+++ src/main/java/cl/usm/tallerhdd/TallerHddApplication.java
@@ -0,0 +1,13 @@
+package cl.usm.tallerhdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerHddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TallerHddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/usm/tallerhdd/TallerHddApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usm.tallerhdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerHddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
